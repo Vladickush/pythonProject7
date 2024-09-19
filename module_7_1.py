5a759703589c1e566fa026e905b0abf5df3e98df
Vladickush <u.vn@mail.ru>
2024-09-19 23:22:13 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,40 +1,35 @@
-#Задача "Учёт товаров":
+# Задача "Учёт товаров":
 class Product:
 
-    def __init__(self, name=str, weight=float, category=str):
-        self.name = name
-        self.weight = weight
-        self.category = category
+    def __init__(self, name, weight, category):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.category = str(category)
 
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
 
 
 class Shop:
-    __file_name = 'products.txt'
-    str_file = ''
+    def __init__(self):
+        self.__file_name = 'products.txt'
 
     def get_products(self):
         file = open(self.__file_name, 'r')
-        self.str_file = file.read()
+        products = file.read()
         file.close()
-        return self.str_file
+        return products
 
     def add(self, *products):
+        current_products = self.get_products()
         file = open(self.__file_name, 'a')
-        self.get_products()
-        if self.str_file == '':
-            for i in products:
-                file.write(str(i) + '\n')
-            file.close()
-        else:
-            for i in products:
-                if str(i.name) in self.str_file:
-                    print(f'Продукт {i} уже есть в магазине')
-                    continue
-                else:
-                    file.write(str(i) + '\n')
-            file.close()
+        for product in products:
+            if str(product) not in current_products:
+                file.write(str(product) + '\n')
+                current_products += str(product) + '\n'
+            else:
+                print(f'Продукт {product} уже есть в магазине')
+        file.close()
 
 
 s1 = Shop()
@@ -47,3 +42,4 @@ print(p2)  # __str__
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
+
